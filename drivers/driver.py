7daac7801c5713037c8a54a71e776da1e3c041f3
yura702007 <82802568+yura702007@users.noreply.github.com>
2021-09-24 18:34:30 +0300

    class Driver add

---
+++ drivers/driver.py
@@ -0,0 +1,12 @@
+import time
+
+
+class Driver:
+
+    def __init__(self, name='Александр Рыбчинский', car='Renaut'):
+        self.name = name
+        self.car = car
+
+
+if __name__ == '__main__':
+    dr = Driver()
