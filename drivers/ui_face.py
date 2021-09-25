96bfcdb68ec5befe8ae1c75f1e80c06e7aafe53b
yury702007dev <90982097+yury702007dev@users.noreply.github.com>
2021-09-25 16:04:57 +0300

    drive_for_client_method add

--- drivers/ui_face.py
+++ drivers/ui_face.py
@@ -7,11 +7,21 @@ from driver import Driver
 class Container(BoxLayout):
     adress_call = ObjectProperty()
     start_job = ObjectProperty()
+    drive_for_client = ObjectProperty
 
     dr = Driver()
 
     def start_job_method(self):
+        """
+        Ожидаю вызова
+        """
         self.adress_call.text = str(self.dr.start_job())
+
+    def drive_for_client_method(self):
+        """
+        Еду за пассажиром
+        """
+        self.adress_call.text = str(self.dr.drive_for_client())
 
 
 class FaceApp(App):
